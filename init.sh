f4d9cf66cda8918b917e8ac0f33eca15dcb3d836
wanderer <mjbecze@gmail.com>
2015-12-11 18:58:55 +0000

    use dev branch

--- init.sh
+++ init.sh
@@ -2,7 +2,8 @@
 mkdir tests
 cd tests
 git init
-git remote add origin  https://github.com/ethereum/tests
+git remote add -t develop origin https://github.com/ethereum/tests
+git checkout -b develop
 git config core.sparseCheckout true
 # echo "TransactionTests/*" >> .git/info/sparse-checkout
 # git pull --depth=1 origin master

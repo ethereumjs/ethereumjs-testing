f420131c8d64ca919ead583c0cdc8b5b10eca72f
wanderer <mjbecze@gmail.com>
2015-12-11 00:28:01 +0000

    make travis happy

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 mkdir tests
 cd tests
 git init
-git remote add origin git@github.com:ethereum/tests.git
+git remote add origin  https://github.com/ethereum/tests
 git config core.sparseCheckout true
 # echo "TransactionTests/*" >> .git/info/sparse-checkout
 # git pull --depth=1 origin master

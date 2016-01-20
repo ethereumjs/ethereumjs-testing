b1efcabfeecd0ae0469f6be7af4962dbb269919e
wanderer <mjbecze@gmail.com>
2016-01-19 23:43:55 -0500

    update browserify transform

--- bulk-transform.js
+++ bulk-transform.js
@@ -1,26 +1,8 @@
 const path = require('path')
-const child_process = require('child_process')
-const fs = require('fs')
 const bulkify = require('bulkify')
 
 module.exports = function (file) {
   var testName = process.env.ethTest
-  try {
-    // pull only if the directory is not there
-    fs.statSync(__dirname + '/tests/' + testName)
-  } catch (e) {
-    child_process.execSync('echo "' + testName + '/**" >> .git/info/sparse-checkout', {
-      cwd: __dirname + '/tests'
-    })
-    child_process.execSync('git pull --depth=1', {
-      stdio: [0, 1, 2],
-      cwd: __dirname + '/tests'
-    })
-    child_process.execSync('git reset --hard', {
-      stdio: [0, 1, 2],
-      cwd: __dirname + '/tests'
-    })
-  }
   var filedir = path.dirname(file)
   var opts = {
     vars: {

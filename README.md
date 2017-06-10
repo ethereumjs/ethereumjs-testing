2f338f9737e759eb34cf8ac39e3bf06defe25e44
cdetrio <cdetrio@gmail.com>
2017-06-10 18:32:40 +0200

    use new BlockchainTests/GeneralStateTests format

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 testing utilities for the ethereumjs stack
 
 Uses the offical [Ethereum Tests](https://github.com/ethereum/tests)
+
+To fetch the latest tests:
+```
+git submodule init
+git submodule update
+cd tests
+git pull origin develop
+```
--- index.js
+++ index.js
@@ -11,8 +11,13 @@ const path = require('path')
 const getTests = exports.getTests = (testType, onFile, fileFilter = /.json$/, skipFn = () => {
   return false
 }) => {
+  if (testType === 'BlockchainTests') {
+    // currently maintained BlockchainTests are located in BlockchainTests/GeneralStateTests
+    // e.g. https://github.com/ethereum/tests/tree/e17e44a002e4c602e56486663244b74d8dbbff54/BlockchainTests/GeneralStateTests
+    testType += '/GeneralStateTests'
+  }
   return new Promise((resolve, reject) => {
-    dir.readFiles(path.join(__dirname, 'lintedTests', testType), {
+    dir.readFiles(path.join(__dirname, 'tests', testType), {
       match: fileFilter
     }, async (err, content, fileName, next) => {
       if (err) reject(err)
@@ -21,9 +26,7 @@ const getTests = exports.getTests = (testType, onFile, fileFilter = /.json$/, sk
       const tests = JSON.parse(content)
 
       for (let testName in tests) {
-        if (skipFn(testName)) {
-          next()
-        } else {
+        if (!skipFn(testName)) {
           await onFile(fileName, testName, tests[testName])
         }
       }
@@ -36,20 +39,22 @@ const getTests = exports.getTests = (testType, onFile, fileFilter = /.json$/, sk
 }
 
 exports.getTestsFromArgs = function (testType, onFile, args = {}) {
-  let fileFilter, testFn
+  let fileFilter, skipFn
 
   if (args.file) {
     fileFilter = new RegExp(args.file)
   }
 
   if (args.test) {
-    testFn = (testName) => testName !== args.test
+    skipFn = (testName) => {
+      return (args.skipFn(testName) || testName !== args.test)
+    }
   } else {
-    testFn = args.testFn
+    skipFn = args.skipFn
   }
-  return getTests(testType, onFile, fileFilter, testFn)
+  return getTests(testType, onFile, fileFilter, skipFn)
 }
 
 exports.getSingleFile = (file) => {
-  return require(path.join(__dirname, 'lintedTests', file))
+  return require(path.join(__dirname, 'tests', file))
 }
